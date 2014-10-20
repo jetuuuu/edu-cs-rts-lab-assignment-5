d2cca228f45ddda44e1e801dab20b31a960a2caa
Jetuuuu <galnikrom@gmail.com>
2014-10-20 19:01:07 +0400

    Commit
    Done

--- lab-5-1.c
+++ lab-5-1.c
@@ -3,36 +3,37 @@
 #include <rtdk.h>
 
 void task_body(void *cookie) {
-
-  RT_TASK *current_task;
-  RT_TASK_INFO current_task_info;
-
-  rt_printf("Hello, world\n");
-
-  current_task = rt_task_self();
-  rt_task_inquire(current_task, &current_task_info);
-
-  rt_printf("Task name: %s\n", current_task_info.name);
-
+    
+    RT_TASK *current_task;
+    RT_TASK_INFO current_task_info;
+    
+    rt_printf("Hello, world\n");
+    // Получить информацию о текущей задаче
+    current_task = rt_task_self();
+    // Получить информацию о состоянии текущей задачи
+    rt_task_inquire(current_task, &current_task_info);
+    // Напечатать имя задачи
+    rt_printf("Task name: %s\n", current_task_info.name);
+    
 }
 
 int main(int argc, char **argv) {
-
-  RT_TASK task;
-  int err;
-
-  rt_print_auto_init(1);
-
-  mlockall(MCL_CURRENT|MCL_FUTURE);
-
-  rt_printf("Starting task\n");
-
-  err = rt_task_create(&task, "Lab5Task-1", 0, 50, 0);
-
-  if (!err) {
-    rt_task_start(&task, &task_body, NULL);
-  }
-
-  rt_task_join(&task);
-
+    // Структура данных с информацией о задаче
+    RT_TASK task;
+    int err;
+    // Инициализировать подсистему печати xenomai
+    rt_print_auto_init(1);
+    // Залочить текущие и будущие страницы памяти процесса
+    mlockall(MCL_CURRENT|MCL_FUTURE);
+    // Печатать при помощи функции из real-time printing library
+    rt_printf("Starting task\n");
+    // Создать новую real-time задачу La5Task-1 с приоритетом 50
+    err = rt_task_create(&task, "Lab5Task-1", 0, 50, 0);
+    // Запустить задачу в случае успешного создания
+    if (!err) {
+        rt_task_start(&task, &task_body, NULL);
+    }
+    // Дождаться завершения работы задачи
+    rt_task_join(&task);
+    
 }
--- lab-5-4.c
+++ lab-5-4.c
@@ -2,9 +2,8 @@
 #include <sys/mman.h>
 #include <native/task.h>
 #include <rtdk.h>
-
+#include <pthread.h>
 volatile long int shared_resource = 0L;
-pthread_mutex_t my_gasu_mutex;
 
 #define ITERATIONS 100000
 #define TASKS 10
@@ -16,24 +15,19 @@ void task_body(void *cookie) {
   RT_TASK_INFO current_task_info;
 
   current_task = rt_task_self();
+  //получаем информацию о таске
   rt_task_inquire(current_task, &current_task_info);
 
   rt_printf("Task name: %s started with priority %d\n",
-    current_task_info.name,
+      current_task_info.name,
     current_task_info.cprio
   );
-
   for (int i = 0; i < ITERATIONS; i++) {
-    pthread_mutex_lock(&my_gasu_mutex);
-    long int r = shared_resource;
-    r = r + 1;
-    shared_resource = r;
-    pthread_mutex_unlock(&my_gasu_mutex);
+    __sync_fetch_and_add(&shared_resource, 1);
     rt_task_sleep(DELAY);
   }
-
+ // pthread_mutex_unlock(&my_gasu_mutex);
   rt_printf("Task name: %s is shutting down\n", current_task_info.name);
-
 }
 
 int main(int argc, char **argv) {
@@ -47,7 +41,7 @@ int main(int argc, char **argv) {
 
   for(int i = 0; i < TASKS; i++) {
     snprintf(task_name[i], 16, "Lab5Task-%d", i);
-    if (rt_task_create(&task[i], task_name[i], 0, 50, 0) != 0) {
+    if (rt_task_create(&task[i], task_name[i], 0, 50 - (i*2), T_JOINABLE) != 0) {
       exit(-1);
     }
   }
